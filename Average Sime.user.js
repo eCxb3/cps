df8de5803e41ab579aae69e732209af2ba794384
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2022-11-04 23:45:58 +0200

    Update Average Sime.user.js
    Added the ability to see the number of grades to improve your score

--- Average Sime.user.js
+++ Average Sime.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Average Sime
 // @namespace    http://tampermonkey.net/
-// @version      2077v.1
+// @version      2077v.2
 // @description  Cyberpunk Coding
 // @author       ezX
 // @match        https://agenda.sime.md/ords/f?p=*
@@ -40,6 +40,66 @@ _________        ___.                                     __
         return finalstate.sum / finalstate.count;
     }([2, , , 6]);
 
+    function nextnote(arr, avg) {
+        let nextn = Math.floor(avg);
+        nextn++
+
+        let x = arr.slice();
+        while (average(x) < nextn) {
+            let i = nextn;
+            while (i < 10) {
+                i++
+                x.push(i)
+                if (average(x) < nextn) {
+                    if (i != 10) {
+                        x.splice(x.length--, 1);
+                    }
+                } else {break}
+            }
+        }
+        x.splice(0, arr.length);
+        return x;
+    }
+
+    function editnotelist(btnid) {
+        let notes = document.querySelectorAll('tbody')[26].childNodes[btnid];
+        if (parseInt($(notes).find('td')[2].innerHTML) >= 9) {
+            return alert('Вам не поднять свою оценку');
+        }
+
+        let note_arr;
+
+        try {
+            let note = $(notes).find('td')[1];
+            note = $(note).find('span');
+            let both_notes = note[0].innerHTML+note[1].innerHTML;
+            note_arr = both_notes.split(', ');
+        } catch {
+            note_arr = $(notes).find('td')[1].innerHTML.split(', ');
+        }
+
+        let nn = nextnote(note_arr.map(Number), parseInt($(notes).find('td')[2].innerHTML));
+        //return alert('Вам нужно: ' + nn);
+
+        let old_notes = $(notes).find('td')[1];
+        let sp_old = document.createElement('span');
+        sp_old.innerHTML = old_notes.innerHTML;
+        $(old_notes).empty();
+        old_notes.append(sp_old);
+        let sp_new = document.createElement('span');
+        sp_new.innerHTML = ', '+nn.join(', ');
+        sp_new.style.color = 'red';
+        old_notes.append(sp_new);
+
+
+        let new_avgavg = note_arr.slice().map(Number);
+        Array.prototype.push.apply(new_avgavg,nn);
+        $(notes).find('td')[2].innerHTML = average(new_avgavg);
+        $(notes).find('td')[2].style.color = 'red';
+
+        return;
+    }
+
     try {
         let avg_i = document.createElement('th');
         avg_i.className = 't-Report-colHead';
@@ -51,7 +111,9 @@ _________        ___.                                     __
 
         let lessons = document.querySelectorAll('tbody')[26].childNodes;
         let avgavg = [];
+        let count = -1;
         for (let lesson of lessons) {
+            count++
             if (lesson.nodeName == 'TR') {
                 let childsec = $(lesson).children()[0];
                 if ($(childsec).children()[0].innerHTML != 'Educație pentru societate' ) {
@@ -64,9 +126,13 @@ _________        ___.                                     __
 
 
                         $(lesson).append(note);
-                        //let btnplus = document.createElement('button');
-                        //btnplus.innerHTML = '+';
-                        //$(lesson).append(btnplus);
+                        let btnplus = document.createElement('a');
+                        btnplus.href='#';
+                        btnplus.innerHTML = '+';
+                        btnplus.className = 't-Report-cell';
+                        btnplus.id = 'EDITNLIST:' + count;
+                        btnplus.onclick = function() { editnotelist(btnplus.id.split(':')[1]);};
+                        $(lesson).append(btnplus);
                     }
                 }
             }
